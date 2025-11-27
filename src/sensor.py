5118bb733213ea2d0bca4bfb63f1ca591afdb771
Xiaohu530 <J272135@tafe.wa.edu.au>
2025-11-27 10:20:02 +0800

    create display and sensor class

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,15 @@
 class Sensor:
+  def __init__(self, id, is_active=False, car_park=None):
+    self.id = id
+    self.is_active = is_active
+    self.car_park = car_park
+    
+  def __str__(self):
+    status = "ON" if self.is_active else "OFF"
+    return f"{self.id}: {status}"
+  
+class EntrySensor(Sensor):
+  pass
+
+class ExitSensor(Sensor):
   pass
